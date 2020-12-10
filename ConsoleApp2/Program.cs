1da082177c07f00538afd305fbe5fc1852fb528a
Daniil202002 <75783927+Daniil202002@users.noreply.github.com>
2020-12-10 16:51:21 +0300

    First commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 7)
+            {
+                Console.WriteLine("Привет");
+            }
+            else
+            {
+                Console.WriteLine("Не правильно введено число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ConsoleApp1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            // Способы создания экземпляра типа string
+            string i;
+            Console.WriteLine("Введите имя:");
+            i = Convert.ToString(Console.ReadLine());
+            if (i=="Вячеслав") 
+                {
+                Console.WriteLine("Привет, Вячеслав");
+            }
+            else
+                Console.WriteLine("Нет такого имени");
+
+                
+                Console.ReadKey();
+            }
+        }
+    }
+
+    
+
